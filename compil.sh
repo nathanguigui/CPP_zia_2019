7752413a3f6421039e1d43d6fce10a3a7a31ba4c
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-01-16 00:24:59 +0100

    fix linux compilation

--- compil.sh
+++ compil.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+rm -rf build
 mkdir build
 cd build
 conan install ..
-cmake -G  "Visual Studio 16 2019" ..
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  cmake ..
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  cmake -G  "Visual Studio 16 2019" ..
+elif [[ "$OSTYPE" == "win32" ]]; then
+  cmake -G  "Visual Studio 16 2019" ..
+fi
 cmake --build . --config Release
