196105a42faa856140875020679d04286efbf249
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-01-16 00:54:17 +0100

    :white_check_mark: print jsoncpp version
    print jsoncpp verision

--- core/main.cpp
+++ core/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <boost/version.hpp>
 //#include <jsoncpp/json/version.h>
+#include <json/version.h>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Using Boost "     
-          << BOOST_VERSION / 100000     << "."  // major version
-          << BOOST_VERSION / 100 % 1000 << "."  // minor version
-          << BOOST_VERSION % 100                // patch level
-          << std::endl;
-    //std::cout << "Using JsonCpp " << JSONCPP_VERSION_STRING
+    std::cout << "Using Boost " << BOOST_VERSION / 100000 << "." << BOOST_VERSION / 100 % 1000 << "." << BOOST_VERSION % 100 << std::endl;
+    std::cout << "Using JsonCpp " << JSONCPP_VERSION_STRING << std::endl;
 }
