5d4f477df5b4c1c3413a8c0e37a06b66dbc899db
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-02-04 15:31:33 +0100

    :sparkles: windows php
    windows php

--- src/modules/PhpModule/PhpModule.cpp
+++ src/modules/PhpModule/PhpModule.cpp
@@ -16,21 +16,6 @@ void PhpModule::handleRequest(HttpRequest &request) {
 
 void PhpModule::handlePreResponse(HttpRequest &request, HttpResponse &response) {
     std::string command = "php " + response.filePath;
-    /*//FILE *result;
-    char *buffer;
-    #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-        result = _popen(command.c_str(), "r");
-    #else
-        result = popen(command.c_str(), "rw");
-    #endif
-    if (result != nullptr) {
-        fseek(result, 1, SEEK_END);
-        size_t size = ftell(result);
-        rewind(result);
-        buffer = (char*) malloc(sizeof(char)*size);
-        if (fread(buffer, 1, size, result) == size)
-            response.body = buffer;
-    }*/
     response.body = PhpModule::exec(command.c_str());
 }
 
@@ -41,7 +26,11 @@ void PhpModule::handlePreStart(std::vector<ServerInstanceConfig> &instances) {
 std::string PhpModule::exec(const char *cmd) {
     std::array<char, 128> buffer;
     std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+        std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+    #else
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    #endif
     if (!pipe)
         throw std::runtime_error("popen() failed!");
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
