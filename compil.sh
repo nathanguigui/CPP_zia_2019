421142c0777a788618e095167cd2de4fb872df5e
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-02-05 15:54:56 +0100

    fix config copy

--- compil.sh
+++ compil.sh
@@ -3,23 +3,22 @@ if [ $# -eq 2 ]; then
   rm -rf build
   mkdir build
 fi
-cd build
-conan install --build=missing ..
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   rm -rf config
   cp -R configLinux config
-  cmake ..
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-  rm -rf config
-  cp -R configWindows config
-  cmake -G  "Visual Studio 16 2019" ..
-elif [[ "$OSTYPE" == "win32" ]]; then
-  rm -rf config
-  cp -R configWindows config
-  cmake -G  "Visual Studio 16 2019" ..
 else
   rm -rf config
   cp -R configWindows config
+fi
+cd build
+conan install --build=missing ..
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  cmake ..
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  cmake -G  "Visual Studio 16 2019" ..
+elif [[ "$OSTYPE" == "win32" ]]; then
+  cmake -G  "Visual Studio 16 2019" ..
+else
   cmake -G  "Visual Studio 16 2019" ..
 fi
 cmake --build . --config Release
