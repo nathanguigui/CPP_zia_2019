1de87cedfefb70e1855faf7946ee269d75e6aedb
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-02-05 14:37:42 +0100

    change default config arch

--- compil.sh
+++ compil.sh
@@ -6,12 +6,20 @@ fi
 cd build
 conan install --build=missing ..
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  rm -rf config
+  cp -R configLinux config
   cmake ..
 elif [[ "$OSTYPE" == "cygwin" ]]; then
+  rm -rf config
+  cp -R configWindows config
   cmake -G  "Visual Studio 16 2019" ..
 elif [[ "$OSTYPE" == "win32" ]]; then
+  rm -rf config
+  cp -R configWindows config
   cmake -G  "Visual Studio 16 2019" ..
 else
+  rm -rf config
+  cp -R configWindows config
   cmake -G  "Visual Studio 16 2019" ..
 fi
 cmake --build . --config Release
